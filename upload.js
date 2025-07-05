6e95f1dcc6de2d24ab27060d3aea7ef0fbd466fe
RehamanSyed979 <rehamansyed979@gmail.com>
2025-07-05 22:45:59 +0530

    Check-in #2.5

--- upload.js
+++ upload.js
@@ -16,7 +16,7 @@ const upload = multer({
   storage: multerS3({
     s3,
     bucket: process.env.AWS_S3_BUCKET,
-    acl: 'public-read',
+    // acl: 'public-read',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: function (req, file, cb) {
       const ext = file.originalname.split('.').pop();
