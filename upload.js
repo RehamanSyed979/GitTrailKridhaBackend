6cb337642943d64e9bc76c9f3c26512841d15146
RehamanSyed979 <rehamansyed979@gmail.com>
2025-07-05 20:04:25 +0530

    Check-In #2.2

--- upload.js
+++ upload.js
@@ -8,10 +8,8 @@ const router = express.Router();
 
 const s3 = new AWS.S3({
   region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  }
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
 const upload = multer({
